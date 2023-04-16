efad8f664e71fa5c421f701f5181b30f325ef7fa
melmln <melanymolinaverdun@gmail.com>
2023-04-16 18:36:09 -0300

    Estructura y estilos Header e Intro components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import './Header.scss';
-
  
 const Header = () => {
   return (
     <>
       <nav>
-          <div className="logo">
-          </div>
+          <div className="burger-menu">Menu</div>
           <button className="hello-btn">Say hello</button>
       </nav>
     </>
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,5 @@
 import './Intro.scss';
+import avatar from '../../assets/avatar.png'
 
 const Intro = () => {
   return (
@@ -9,7 +10,19 @@ const Intro = () => {
               <h1 className="my-name">Melany Molina Verdún</h1>
               <h4 className="profile">I'm a <span className='front-dev'>frontend developer</span></h4>
             </div>
-            <img src='' alt="avatar" className="avatar" />
+
+            <div className="avatar-container">
+              <img src={avatar} alt="avatar" className="avatar" />
+            </div>
+
+            <button className="resume-btn">Resume</button>
+        
+            <div className="social-networks-container">
+              <div className="line"></div>
+              <div className="social-networks">Icons</div>
+              <div className="line"></div>
+            </div>
+            
         </div>
     </>
   )
