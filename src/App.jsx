f349912d370fee344f4d08eddc254c435fc4d289
melmln <melanymolinaverdun@gmail.com>
2023-04-17 20:26:32 -0300

    Estructura y estilos del componente ProjectWindow

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Intro from './Pages/Intro/Intro';
 import About from './Pages/About/About';
 import Technologies from './Pages/Technologies/Technologies'
-import Projects from './Pages/Projects/Projects';
+import ProjectWindow from './components/ProjectWindow/ProjectWindow';
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
       <Intro/>
       <About/>
       <Technologies/>
-      <Projects/>
+      <ProjectWindow/>
     </>
   )
 }
---
+++ src/components/ProjectWindow/ProjectWindow.jsx
@@ -0,0 +1,48 @@
+import './ProjectWindow.scss'
+import projects from '../../utils/projects.json'
+
+const ProjectWindow = () => {
+  return (
+    <>  {projects.map((project) => {
+
+        const {id, title, img, description, type} = project;
+            
+        return (
+
+                <div className="project-window">
+                    <div className="number">{id}</div>
+
+                    <div className="window">
+                        <div className="project-title-container">
+                            <h1 className="title">{title}</h1>
+
+                            <div className="title-btn-container">
+                                <button className={`visit-btn ${title}`}>Visit</button>
+                                <button className="code-btn">Code</button>
+                            </div>
+                        </div>
+
+                        <div className="project-img-container">
+                            <img 
+                            src={`src/assets/img/Projects/${img}`} 
+                            alt="project-img" 
+                            className="project-img" />
+                        </div>
+
+                        <div className="project-info-container">
+                            <div className="type">{type}</div>
+                            <div className="description">{description}</div>
+                        </div>
+
+                    </div>
+                </div>
+            )
+    })
+    
+        
+        }
+    </>
+  )
+}
+
+export default ProjectWindow;
