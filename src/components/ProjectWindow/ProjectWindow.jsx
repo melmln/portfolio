134ab9725bcbb22ba30c8f5bc51a6ee6bd7639cd
melmln <melanymolinaverdun@gmail.com>
2023-04-19 14:51:15 -0300

    Actualizo rutas img en ProjectWindow

--- src/components/ProjectWindow/ProjectWindow.jsx
+++ src/components/ProjectWindow/ProjectWindow.jsx
@@ -24,7 +24,7 @@ const ProjectWindow = () => {
 
                         <div className="project-img-container">
                             <img 
-                            src={`src/assets/img/Projects/${img}`} 
+                            src={`../../src/assets/img/Projects/${img}`} 
                             alt="project-img" 
                             className="project-img" />
                         </div>
